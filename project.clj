b6c79083f7cd6ea28017668f4c61c20205296aa8
Colin Mitchell <muffinista@gmail.com>
2022-05-30 11:56:07 -0400

    Add gitignore. Add project updates

--- project.clj
+++ project.clj
@@ -1,5 +1,14 @@
-(defproject overtone-tutorial "1.0.0"
-  :dependencies [[overtone/overtone "0.10.5"]]
+(defproject rand-audio-book "1.0.0"
+  :description "Audio book of the RAND corp random number list"
+  :url "http://muffinlabs.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    [org.clojure/clojure "1.11.1"]
+    [org.clojure/tools.cli "1.0.206"]
+    [overtone/overtone "0.10.5"]
+  ]
+  :main rand-audio-book/core
   :native-path "native"
   :source-paths ["src"])
   
