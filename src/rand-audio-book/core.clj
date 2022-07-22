ea3ac9d299f9561be3728b54664e27836c15e0cc
Colin Mitchell <muffinista@gmail.com>
2022-07-22 17:08:00 -0400

    Add rough code

--- src/rand-audio-book/core.clj
+++ src/rand-audio-book/core.clj
@@ -1,23 +1,27 @@
 (ns rand-audio-book.core
   (:require [clojure.tools.cli :refer [parse-opts]])
   (:require [clojure.string :as str])
-  (:require [clojure.data.json :as json])
+  ;; (:require [clojure.data.json :as json])
   (:require [clojure.java.io :as io])
   ;; (:use overtone.live)
   ;; ;;(:use overtone.core)
   (:gen-class)
   )
 
-(defn read-digits [src]
-  (let [
-    text (slurp src)
-  ]
-  text
-  )  
-)
+(def voice-dir "voices")
+(def long-delay 2000)
+(def short-delay 500)
 
-(defn parse-lines [text]
-  (clojure.string/split-lines text)  
+;; given a voice and a letter, return the path to the sample
+(defn path-to-sample [datadir voice text]
+  (let [
+        dest (io/file datadir voice-dir)
+        voice-dest (io/file dest voice)
+        src (str (md5 text) ".wav")
+        filename (.toString (io/file voice-dest src))
+        ]
+    filename
+    )
   )
 
 ;; (first (parse-lines (read-digits "data/digits.txt")))
@@ -27,41 +31,57 @@
 ;; (clojure.string/split (first (parse-lines (read-digits "data/digits.txt"))) #"\s+")
 ;; ["00000" "10097" "32533" "76520" "13586" "34673" "54876" "80959" "09117" "39292" "74945"]
 
+;;
+;; load in the source digits file
+;;
+(defn read-digits [src]
+  (let [
+    text (slurp src)
+  ]
+  text
+  )  
+)
+
+;;
+;; split text file into lines
+;;
+(defn parse-lines [text]
+  (clojure.string/split-lines text)  
+  )
+
+;;
+;; split a line into list of component parts
+;;
 (defn line-groupings [line]
   (clojure.string/split line #"\s+")
   )
 
+;;
+;; get the index of a line
+;; ["00000" "10097" "32533" "76520" "13586" "34673" "54876" "80959" "09117" "39292" "74945"]
+;; => "00000"
+;;
 (defn line-index [line]
   (first (line-groupings line))
   )
 
+;;
+;; get the data of a line
+;; ["00000" "10097" "32533" "76520" "13586" "34673" "54876" "80959" "09117" "39292" "74945"]
+;; => ["10097" "32533" "76520" "13586" "34673" "54876" "80959" "09117" "39292" "74945"]
+;;
 (defn line-data [line]
   (rest (line-groupings line))
   )
-  
-;; ;; given a voice and the text to be spoken, return the
-;; ;; path where we expect the sample to exist
-;; (defn path-to-sample [datadir voice text]
-;;   (let [
-;;         dest (io/file datadir voice-dir)
-;;         voice-dest (io/file dest voice)
-;;         src (str (md5 text) ".wav")
-;;         filename (.toString (io/file voice-dest src))
-;;         ]
-;;     filename
-;;     )
-;;   )
 
-;; ;; given a voice and a letter, return the path to the sample
-;; ;; NOTE: this is path-to-sample but without the md5 hash, so
-;; ;; these two functions can probably be refactored
-;; (defn path-to-letter [datadir voice text]
-;;   (let [
-;;         dest (io/file datadir voice-dir)
-;;         voice-dest (io/file dest voice)
-;;         src (str text ".wav")
-;;         filename (.toString (io/file voice-dest src))
-;;         ]
-;;     filename
-;;     )
-;;   )
+;;
+;; speak the contents of a line
+;;
+(defn speak-line [line]
+  (prn (line-index line))  
+  )
+
+
+(doseq [line (parse-lines (read-digits "data/digits.txt"))]
+    (speak-line line)
+  )
