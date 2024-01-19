54ea51b3060a1cb9b4a6e3855172400c83759af6
Coin Mitchell <colin@muffinlabs.com>
2024-01-19 16:54:16 +0000

    Output everything

--- create-book.py
+++ create-book.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+# docker run --rm -it -v "$PWD":/usr/src/app rand-audio ./sample.py
+
+
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
@@ -8,43 +11,74 @@ load_dotenv()  # take environment variables from .env.
 import pathlib
 import os
 import time
-# from pydub import AudioSegment
 import wave
+import random
 
 sample_dir = "samples/"
 output_dir = "output/"
 
+# https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
+def chunks(l, n): 
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
+
+def pause(dest, count):
+    # add a little wiggle but make it even
+    wiggle = random.randrange(-1000, 1000) * 2
+    blank = bytearray(count + wiggle)
+    output.writeframes(blank)    
+
 pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-with open("data/short.txt", encoding="utf-8") as f:
+with open("data/digits.txt", encoding="utf-8") as f:
     read_data = f.read()
 
 # https://stackoverflow.com/questions/65949790/how-to-append-audio-frames-to-wav-file-python
 
-output = wave.open(os.path.join(output_dir, "result.wav"), 'wb')
-first = True
 
-lines = read_data.split("\n")
-for line in lines:
-    phrases = line.split()
-    for phrase in phrases:
-      filename = os.path.join(sample_dir, phrase + ".wav")
-      print(filename)
+chapter_size = 1000
+all_lines = read_data.split("\n")
 
-      sample = wave.open(filename, 'rb')
-      params = sample.getparams()
-      frames = sample.readframes(sample.getnframes())
+# remove blank lines
+while("" in all_lines):
+    all_lines.remove("")
 
-      # only do this once!
-      if first:
-          output.setparams(params)
-          first = False
+chapters = chunks(all_lines, 1000)
 
-      output.writeframes(frames)
+count = 1
+for lines in chapters:
+    first = True
 
-      sample.close()
+    print(count)
+    print(lines)
+    output = wave.open(os.path.join(output_dir, "chapter" + str(count) + ".wav"), 'wb')
 
-      print("quick break")
-    print("add pause")
+    for line in lines:
+        phrases = line.split()
+        for phrase in phrases:
+            filename = os.path.join(sample_dir, phrase + ".wav")
+            print(filename)
 
-output.close()
+            sample = wave.open(filename, 'rb')
+            params = sample.getparams()
+            frames = sample.readframes(sample.getnframes())
+
+            # only do this once!
+            if first:
+                output.setparams(params)
+                first = False
+
+            output.writeframes(frames)
+
+            sample.close()
+
+            print("quick break")
+            pause(output, 10000)
+
+
+        print("add pause")
+        pause(output, 30000)
+
+    output.close()
+    count = count + 1
